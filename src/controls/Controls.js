70310cf35e8ee21726077a5d9c38c47500cbd6f3
bamfranceschi <anna.franceschi@gmail.com>
2019-12-16 19:53:18 -0600

    13 tests written

--- src/controls/Controls.js
+++ src/controls/Controls.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React from "react";
 
 const Controls = props => {
   const { locked, closed, toggleLocked, toggleClosed } = props;
 
   return (
     <div className="controls panel">
-      <button disabled={!closed} onClick={toggleLocked} className="toggle-btn">
-        {locked ? 'Unlock Gate' : 'Lock Gate'}
+      <button
+        disabled={!closed}
+        onClick={toggleLocked}
+        className="toggle-btn"
+        data-testid={"toggle_locked"}
+      >
+        {locked ? "Unlock Gate" : "Lock Gate"}
       </button>
-      <button disabled={locked} onClick={toggleClosed} className="toggle-btn">
-        {closed ? 'Open Gate' : 'Close Gate'}
+      <button
+        disabled={locked}
+        onClick={toggleClosed}
+        className="toggle-btn"
+        data-testid={"toggle_closed"}
+      >
+        {closed ? "Open Gate" : "Close Gate"}
       </button>
     </div>
   );
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,22 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Dashboard from "./Dashboard";
+import Controls from "../controls/Controls";
+import Display from "../display/Display";
+
 // Test away
+
+//basic render test
+
+test("testing Dashboard renders", () => {
+  render(<Dashboard />);
+});
+
+test("testing Controls renders", () => {
+  render(<Controls />);
+});
+
+test("testing Display renders", () => {
+  render(<Display />);
+});
